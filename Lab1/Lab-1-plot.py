59b159a7d7cfb8345a41e6224fb3291596b4cadf
rickmagal <rickmagal@debian>
2023-04-02 15:30:57 -0400

    Plotting

--- Lab1/Lab-1-plot.py
+++ Lab1/Lab-1-plot.py
@@ -3,6 +3,7 @@ from multiprocessing.pool import ThreadPool
 import random
 import math
 import time
+import matplotlib.pyplot as plt
 
 def merge(*args):
     left, right = args[0] if len(args) == 1 else args
@@ -39,12 +40,10 @@ def merge_sort_process(data, k):
     data = pool.map(merge_sort, data)
     iteration = 1
     while len(data) > 1:
-        print("Iteration", iteration, data)
         iteration +=1
         extra = data.pop() if len(data) % 2 == 1 else None
         data = [(data[i], data[i + 1]) for i in range(0, len(data), 2)]
         data = pool.map(merge, data) + ([extra] if extra else [])
-    print("Iteration", iteration, data)
     pool.close()
     return data[0] 
 
@@ -55,28 +54,33 @@ def merge_sort_thread(data, k):
     data = pool.map(merge_sort, data)
     iteration = 1
     while len(data) > 1:
-        print("Iteration", iteration, data)
         iteration +=1
         extra = data.pop() if len(data) % 2 == 1 else None
         data = [(data[i], data[i + 1]) for i in range(0, len(data), 2)]
         data = pool.map(merge, data) + ([extra] if extra else [])
-    print("Iteration", iteration, data)
     pool.close()
     return data[0] 
 
 if __name__ == "__main__":
-    n = int(input("Size of vector:"))
-    k = int(input("Number of initial chunks:"))
-    vector_unsorted = [random.randint(0,n) for i in range(n)]
-    print("--------- Process Run -----------------")
-    print("Unsorted vector ", vector_unsorted)
-    start = time.time()
-    vector_sorted = merge_sort_process(vector_unsorted, k)
-    end = time.time() - start
-    print("Sorted Vector: ", vector_sorted, "time: ", end)
-    print("--------- Thread Run -----------------")
-    print("Unsorted vector ", vector_unsorted)
-    start = time.time()
-    vector_sorted = merge_sort_thread(vector_unsorted, k)
-    end = time.time() - start
-    print("Sorted Vector: ", vector_sorted, "time: ", end)
+    k = 16
+    n_data = []
+    time_data_process = []
+    time_data_thread = []
+    for n in range(16,1000):
+        n_data.append(n)
+        vector_unsorted = [random.randint(0,n) for i in range(n)]
+        start = time.time()
+        vector_sorted = merge_sort_process(vector_unsorted, k)
+        end = time.time() - start
+        time_data_process.append(end)
+        start = time.time()
+        vector_sorted = merge_sort_thread(vector_unsorted, k)
+        end = time.time() - start
+        time_data_thread.append(end)
+    plt.plot(n_data, time_data_process, label="Processos")
+    plt.plot(n_data, time_data_thread, label="Threads")
+    plt.xlabel("Tamanho do vetor (n)")
+    plt.ylabel("Tempo (em s)")
+    plt.title("Tempo de execucao vs. Tamanho para k=" + str(k))
+    plt.legend()
+    plt.show()
